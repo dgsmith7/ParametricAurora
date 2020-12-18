18dc176754882b935af639130cd58b0bdc153830
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2020-12-18 17:53:25 -0600

    Changed inital values to random so each run is unique

--- ParametricAurora.js
+++ ParametricAurora.js
@@ -6,7 +6,7 @@
 //   Original code by David Gail Smith
 ////////////////////////////////////////////
 
-    var coefVec = new THREE.Vector3(13, 10, 14);
+    var coefVec = new THREE.Vector3(Math.random(16), Math.random(16), Math.random(16));
   var twn = [];
   var change = true;
   var twnLen = 20000;
